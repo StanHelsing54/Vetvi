b135ea0959533900caa768e7e6c23dd45ec3949a
unknown <StanHelsing54@yandex.ru>
2018-03-15 09:01:12 +0700

    Start

---
+++ PlusVetvi/UmnVetvi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmnVetvi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = A * B;
+            Console.WriteLine("{0}", C);
+            Console.ReadKey();
+        }
+    }
+    }
+}
